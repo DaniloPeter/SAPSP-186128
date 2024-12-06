82013f76339824cc9a0ec2defedf713f16d57057
DaniloPeter <gomerplayer@gmail.com>
2024-12-06 20:02:08 +0300

    overprint done, defect switch todo, downtime ok

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,10 +2,16 @@ sap.ui.define([], () => {
   "use strict";
 
   return {
-    calculateOverprints(meters, reportLength) {
+    calculateOverprints(printMeters, reportLength) {
+      if (!printMeters || !reportLength) return 0;
+      return (printMeters / (reportLength * 0.01)).toFixed(2);
+    },
+
+    toggleWeight(weight, defect) {
+      const isDefect = weight > 50;
       debugger;
-      if (!meters || !reportLength) return 0;
-      return (meters / (reportLength * 0.01)).toFixed(2);
+      // TODO: не понял как передать в модель состояние
+      // switch не работает правильно, если его переключать отдельно
     },
   };
 });
